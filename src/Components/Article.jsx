62ccd78493971faa14f3fc7644c5847bb0baebd6
Christoper Constancio <chris_constancio@hotmail.com>
2023-11-04 11:56:30 -0600

    Sizes

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -23,7 +23,7 @@ export default function Article() {
                         "Sin foto"}
                 </div>
                 {data ?
-                    <div className='bg-gray-200 h-20 w-full sm:hidden'>
+                    <div className=' h-20 w-full sm:hidden'>
                         <p className='font-Geologica text-2xl'>{data.name}</p>
                         <h1 className='font-Geologica text-2xl'> <strong>Precio : </strong> {data.price} </h1>
                         <h1 className='font-Geologica text-2xl'> <strong>Marca : </strong> {data.brand} </h1>
